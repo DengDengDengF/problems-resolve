486aaded946dffc5a3fe29ea740c3c62fb36e28f
DengDengDengF <lidengfenglidengfeng4@gmail.com>
2024-06-05 15:51:13 +0800

    用spark-md5获取哈希，以及实现文件上传、断点续传，添加备注

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <DialogDemo/>
+<!--  <DialogDemo/>-->
 <!--  <PdfDemo/>-->
-  <PdfHeigh/>
+<!--  <PdfHeigh/>-->
+<!--  <FileSplit/>-->
 </template>
 
 <script>
@@ -9,12 +10,13 @@
 import DialogDemo from "./pages/DialogDemo.vue";
 //pdf  传入的方式不是二进制流，比较模糊  参考掘金 https://juejin.cn/post/7105933034771185701
 import PdfDemo from "./pages/PdfDemo.vue";
-//
+//pdf-vue3.0 体验良好
 import PdfHeigh from "./pages/pdfHeigh.vue";
-
+//文件分批上传
+import FileSplit from "./pages/FileSplit.vue";
 export default {
   name: 'App',
-  components: {DialogDemo, PdfDemo,PdfHeigh},
+  components: {DialogDemo, PdfDemo,PdfHeigh,FileSplit},
   setup(props) {
 
   }

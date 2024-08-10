7644b8fad7ded42ce326b36b69e7465190f99963
DengDengDengF <lidengfenglidengfeng4@gmail.com>
2024-08-10 17:55:25 +0800

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,12 @@ import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), copy({
-        targets: [
-            {src: 'node_modules/@liveqing/liveplayer-v3/dist/component/liveplayer-lib.min.js', dest: 'public/js'},
-        ]
-    })]
+    plugins: [
+        vue(),
+        copy({
+            targets: [
+                {src: 'node_modules/@liveqing/liveplayer-v3/dist/component/liveplayer-lib.min.js', dest: 'public/js'},
+            ]
+        })
+    ]
 })

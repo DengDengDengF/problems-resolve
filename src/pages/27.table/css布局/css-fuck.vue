757096986949c66a3ca4e10f6d8bead7aded5805
ldf <lidengfenglidengfeng4@gmail.com>
2025-08-08 23:05:21 +0800

    样式补充2

--- src/pages/27.table/css布局/css-fuck.vue
+++ src/pages/27.table/css布局/css-fuck.vue
@@ -1,9 +1,9 @@
 <template>
-  <div style="width: 100%; height: 400px; border: 1px solid olivedrab; overflow-x: auto; overflow-y: hidden;">
+  <div style="width: 100%; height: 600px; border: 1px solid olivedrab; overflow-x: auto; overflow-y: hidden;">
     <div style="
     display: flex;
     min-width: max-content;
-    height: 100%;
+    height: 50%;
     box-sizing: border-box;
   ">
       <!-- 固定宽度项 -->
@@ -13,7 +13,22 @@
       <!-- 自适应项：有 min-width 和 flex，撑不下就滚动 -->
       <div style="min-width: 200px; flex: 1; border: 1px solid orangered;"></div>
       <div style="min-width: 200px; flex: 1; border: 1px solid saddlebrown;"></div>
-      <div style="min-width: 200px; flex: 1; border: 1px solid green;background: red"></div>
+      <div style="min-width: 800px; flex: 1; border: 1px solid green;background: red"></div>
+    </div>
+    <div style="
+    display: flex;
+    min-width: max-content;
+    height: 50%;
+    box-sizing: border-box;
+  ">
+      <!-- 固定宽度项 -->
+      <div style="width: 300px; height: 100%; border: 1px solid red;"></div>
+      <div style="width: 300px; height: 100%; border: 1px solid blue;"></div>
+
+      <!-- 自适应项：有 min-width 和 flex，撑不下就滚动 -->
+      <div style="min-width: 200px; flex: 1; border: 1px solid orangered;"></div>
+      <div style="min-width: 200px; flex: 1; border: 1px solid saddlebrown;"></div>
+      <div style="min-width: 800px; flex: 1; border: 1px solid green;background: red"></div>
     </div>
   </div>
 
